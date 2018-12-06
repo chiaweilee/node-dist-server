f32d05ae3e1163720fd7429455035a4f02dea2b9
jv <ljw@live.jp>
2018-12-06 11:34:32 +0800

    fix: 🧹 clean

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/strongly-recommended',
-    '@vue/standard'
-  ],
+  'extends': [],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
