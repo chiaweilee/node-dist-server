55e6f3f5060dc7dd5675dfa55376d119a7839dd4
jv <ljw@live.jp>
2018-12-06 11:35:54 +0800

    refactor: 🔥 path & joi validate

--- bin/option.js
+++ bin/option.js
@@ -1,7 +1,18 @@
+const path = require('path')
+const joi = require('joi')
+
+const schema = joi.object().keys({
+  httpPort: joi.number().port(),
+  distPath: joi.string(),
+  defaultPage: joi.string(),
+  extendPath: joi.any()
+})
+
 const options = {
   httpPort: 3000,
+  distPath: path.resolve('./dist/'),
   defaultPage: 'index.html',
-  distPath: '../../../dist/'
+  extendPath: undefined
 }
 
 const argv = process.argv.slice(2)
@@ -10,18 +21,18 @@ argv.forEach((arg, i) => {
   const next = argv[i + 1]
   switch (arg) {
     case '--port':
-      if (!isNaN(argv[i + 1])) {
+      if (next) {
         options.httpPort = next
       }
       break
     case '--dir':
       if (next) {
-        options.distPath = next
+        options.distPath = path.resolve(next)
       }
       break
     case '--dirname':
       if (next) {
-        options.distPath = `../../../${next}/`
+        options.distPath = path.resolve(`./${next}/`)
       }
       break
     case '--index':
@@ -31,11 +42,18 @@ argv.forEach((arg, i) => {
       break
     case '--extend':
       if (next) {
-        options.extendPath = next
+        options.extendPath = path.resolve(next)
       }
       break
     default:
       break
+  }
+})
+
+joi.validate(options, schema, function (err) {
+  if (err) {
+    process.stderr.output(err)
+    process.exit(1)
   }
 })
 
