b58d0d55f40d8a602f42a7484a05bfb4c6e01b9e
jv <ljw@live.jp>
2019-01-19 08:24:53 +0800

    refactor: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   root: true,
+  extends: ['ali'],
   env: {
     node: true
   },
-  'extends': [],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
