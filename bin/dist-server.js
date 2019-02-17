f492466fd3a3a14857f8c2e65e74ac50ce47f89b
+v <ljw@live.jp>
2019-02-17 22:07:04 +0800

    build: merge (#2)
    * refactor: move
    
    * refactor: eslint

--- bin/dist-server.js
+++ bin/dist-server.js
@@ -1,58 +1,58 @@
 #!/usr/bin/env node
 
-const consola = require('consola')
-const ip = require('ip')
-const options = require('./option')
-const { app, server } = require('./app')
-const mimeType = require('./mime-type')
-const { resolve, readFile } = require('./utils')
+const consola = require('consola');
+const ip = require('ip');
+const options = require('../lib/option');
+const { app, server } = require('../lib/app');
+const mimeType = require('../lib/mime-type');
+const { resolve, readFile } = require('../lib/utils');
 
 if (options.extendPath) {
-  const extend = require(options.extendPath)
+  const extend = require(options.extendPath);
   if (typeof extend === 'function') {
-    consola.success(`extend load success at ${options.extendPath}`)
-    extend(app)
+    consola.success(`extend load success at ${options.extendPath}`);
+    extend(app);
   } else {
-    consola.error(`extend load fail at ${options.extendPath}, 'module.exports' must be a function`)
+    consola.error(`extend load fail at ${options.extendPath}, 'module.exports' must be a function`);
   }
 }
 
-app.get('*', function (request, response) {
-  const url = request.originalUrl !== '/' ? request.originalUrl : `/${options.defaultPage}`
-  const [, name, ext] = (() => /\/([^/]+)\.([a-zA-Z0-9]+)$/gi.exec(url.split('?')[0]) || [])()
+app.get('*', (request, response) => {
+  const url = request.originalUrl !== '/' ? request.originalUrl : `/${options.defaultPage}`;
+  const [, name, ext] = (() => /\/([^/]+)\.([a-zA-Z0-9]+)$/gi.exec(url.split('?')[0]) || [])();
   const redirect = function (url) {
-    readFile(resolve(url), function (err, data) {
+    readFile(resolve(url), (err, data) => {
       if (err) {
         if (err.code === 'ENOENT') {
           // try url rewrite for vue-router history mode
-          readFile(resolve(`${options.distPath}${options.defaultPage}`), function (err, data) {
+          readFile(resolve(`${options.distPath}${options.defaultPage}`), (err, data) => {
             if (err) {
-              response.send(err)
-              return
+              response.send(err);
+              return;
             }
-            response.writeHead(200, { 'content-type': 'text/html' })
-            response.write(data, 'utf8')
-            response.end()
-          })
-          return //
+            response.writeHead(200, { 'content-type': 'text/html' });
+            response.write(data, 'utf8');
+            response.end();
+          });
+          return; //
         }
-        response.send(err)
-        return
+        response.send(err);
+        return;
       }
       response.writeHead(200, {
         'Cache-Control': (!(name === 'service-worker' && ext === 'js') && mimeType[ext] && mimeType[ext][2]) || 'no-cache',
-        'content-type': (mimeType[ext] && mimeType[ext][0]) || 'text/plain'
-      })
-      response.write(data, (mimeType[ext] && mimeType[ext][1]) || 'utf8')
-      response.end()
-    })
-  }
-  redirect(`${options.distPath}${url}`, (mimeType[ext] || [])[1])
-})
+        'content-type': (mimeType[ext] && mimeType[ext][0]) || 'text/plain',
+      });
+      response.write(data, (mimeType[ext] && mimeType[ext][1]) || 'utf8');
+      response.end();
+    });
+  };
+  redirect(`${options.distPath}${url}`, (mimeType[ext] || [])[1]);
+});
 
-server.listen(options.httpPort, function () {
-  const isHttps = !!options.https
-  const httpProto = isHttps ? 'https' : 'http'
-  if (!isHttps) consola.success(`Listen at http://localhost:${options.httpPort}/`)
-  consola.success(`Listen at ${httpProto}://${ip.address()}:${options.httpPort}/`)
-})
+server.listen(options.httpPort, () => {
+  const isHttps = !!options.https;
+  const httpProto = isHttps ? 'https' : 'http';
+  if (!isHttps) consola.success(`Listen at http://localhost:${options.httpPort}/`);
+  consola.success(`Listen at ${httpProto}://${ip.address()}:${options.httpPort}/`);
+});
--- bin/utils.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const resolve = _ => path.resolve(__dirname, _)
-const readFile = require('fs').readFile
-
-module.exports = {
-  resolve,
-  readFile
-}
